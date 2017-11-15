b39380665cbfd633c10541a482e3139f50c4be95
Jiri Koutny <koutny@collabim.com>
2017-11-15 08:35:24 +0100

    Speedtest extended

--- speedtest.php
+++ speedtest.php
@@ -4,7 +4,7 @@ include 'functions.php';
 
 ini_set("memory_limit",-1);
 
-foreach ([10, 10e2, 10e3, 10e4, 10e5, 10e6, 10e6*2] as $numberCount) {
+foreach ([10, 10e2, 10e3, 10e4, 10e5, 10e6, 10e6*2, 10e6*4] as $numberCount) {
     printLine(number_format($numberCount) . ' random numbers:');
     printLine('--');
 
