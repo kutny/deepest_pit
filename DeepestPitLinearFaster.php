e2b478fa433d3182e3ee98a1040964b383cd9cc5
Jiri Koutny <koutny@collabim.com>
2017-11-15 08:31:58 +0100

    Function calls removed to speed things up

--- DeepestPitLinearFaster.php
+++ DeepestPitLinearFaster.php
@@ -10,6 +10,7 @@ class DeepestPitLinearFaster
         $startIndex = 0;
         $endIndex = $countA;
 
+        // ořízneme levý okraj pole, pokud není klesající
         for ($i = 0; $i < $countA; $i++) {
             if ($a[$i + 1] >= $a[$i]) {
                 $startIndex = $i + 1;
@@ -19,6 +20,7 @@ class DeepestPitLinearFaster
             }
         }
 
+        // ořízneme pravý okraj pole, pokud není rostoucí
         for ($i = $countA - 1; $i >= 0; $i--) {
             if ($a[$i - 1] >= $a[$i]) {
                 $endIndex = $i - 1;
@@ -33,73 +35,61 @@ class DeepestPitLinearFaster
         }
 
         do {
-            $pitDetails = $this->readPit($a, $startIndex, $countA);
+            $pValue = $lowest = $a[$startIndex];
 
-            $depth = max($pitDetails['depth'], $depth);
-            $startIndex = $pitDetails['newStartIndex'];
+            for ($i = ($startIndex + 1); $i < $countA; $i++) {
+                // aktuální hodnota je nižší, než původní nejnižší
+                if ($a[$i] <= $lowest) {
+                    $lowest = $a[$i];
+
+                    if ($a[$i - 1] == $lowest) {
+                        $pValue = $a[$i];
+                    }
+                }
+                else {
+                    if ($a[$i] > $lowest) {
+                        if ($a[$i - 2] > $a[$i - 1]) { // hodnota předcházející pitu je vyšší, takže pit je reálný
+                            $qValue = $a[$i - 1]; // našli jsme q
+
+                            // hledáme konec pitu
+                            $r = $i;
+                            while ($r <= $countA - 2 && $a[$r + 1] > $a[$r]) {
+                                $r++;
+                            }
+
+                            $rValue = $a[$r];
+
+                            $localDepth = min([$pValue - $qValue, $rValue - $qValue]);
+
+                            $newStartIndex = $r;
+
+                            // po $rValue následuje stejná hodnota => musíme dál hledat $newStartIndex
+                            if ($a[$r + 1] === $a[$r]) {
+                                while ($newStartIndex <= $countA - 2 && $a[$newStartIndex + 1] >= $a[$newStartIndex]) {
+                                    $newStartIndex++;
+                                }
+                            }
+
+                            $depth = max($localDepth, $depth);
+                            $startIndex = $newStartIndex;
+                            continue 2;
+
+                        }
+                        else {
+                            $r = $i;
+                            while ($r <= $countA - 2 && $a[$r + 1] >= $a[$r]) {
+                                $r++;
+                            }
+
+                            $startIndex = $r;
+                            continue 2;
+                        }
+                    }
+                }
+            }
         }
         while ($startIndex < $endIndex - 1);
 
         return $depth;
-    }
-
-    private function readPit(array $a, $startIndex, $countA)
-    {
-        $pValue = $lowest = $a[$startIndex];
-
-        for ($i = ($startIndex + 1); $i < $countA; $i++) {
-            // aktuální hodnota je nižší, než původní nejnižší
-            if ($a[$i] <= $lowest) {
-                $lowest = $a[$i];
-
-                if ($a[$i - 1] == $lowest) {
-                    $pValue = $a[$i];
-                }
-            }
-            else {
-                if ($a[$i] > $lowest) {
-                    if ($a[$i - 2] > $a[$i - 1]) { // hodnota předcházející pitu je vyšší, takže pit je reálný
-                        $qValue = $a[$i - 1]; // našli jsme q
-
-                        // hledáme konec pitu
-                        $r = $i;
-                        while ($r <= $countA - 2 && $a[$r + 1] > $a[$r]) {
-                            $r++;
-                        }
-
-                        $rValue = $a[$r];
-
-                        $depth = min([$pValue - $qValue, $rValue - $qValue]);
-
-                        $newStartIndex = $r;
-
-                        // po $rValue následuje stejná hodnota => musíme dál hledat $newStartIndex
-                        if ($a[$r + 1] === $a[$r]) {
-                            while ($newStartIndex <= $countA - 2 && $a[$newStartIndex + 1] >= $a[$newStartIndex]) {
-                                $newStartIndex++;
-                            }
-                        }
-
-                        return [
-                            'depth' => $depth,
-                            'newStartIndex' => $newStartIndex
-                        ];
-                    }
-                    else {
-                        $r = $i;
-                        while ($r <= $countA - 2 && $a[$r + 1] >= $a[$r]) {
-                            $r++;
-                        }
-
-                        return [
-                            'depth' => null,
-                            'newStartIndex' => $r
-                        ];
-                    }
-                }
-            }
-        }
-
-        throw new \Exception('This should never happen');
     }
 }
